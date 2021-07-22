2a8c4d2307d05962499187750108d8f64494d527
chrisvanetten <christophervanetten@gmail.com>
2021-07-21 19:26:40 -0500

    finished arrays and getRandom functions; still need prompt validation, correct password length

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
 
 function generatePassword() {
  
-  let lowerCase = ['a', 'b', 'c'];
-  let upperCase = ['A', 'B', 'C', 'D'];
-  let specialChar = [];
-  let numbers = [];
+  let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  let specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '?','+'];
+  let number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
   let allchars = lowerCase.concat(upperCase);
+
   var passwordLength = prompt("How long do you want your random password to be? (MIN: 8 characters  MAX: 128 characters)", "")
+  var passwordLower = prompt("Do you want your random password to have lowercase letters?", "")
+  var passwordUpper = prompt("Do you want your random password to have uppercase letters?", "")
+  var passwordSpecial = prompt("Do you want your random password to have special characters?", "")
+  var passwordNumber = prompt("Do you want your random password to have numbers?", "")
 
   let password = [];
+
   function getRandom(arr) {
     let index = Math.floor(Math.random() * arr.length);
     return arr[index];
   }
   /* gets random lowercase letter */
   password.push(getRandom(lowerCase));
+  /* gets random uppercase letter */
   password.push(getRandom(upperCase));
+  /* gets random special character */
+  password.push(getRandom(specialChar));
+  /* gets random number */
+  password.push(getRandom(number));
   /* runs the function 8 times to get a random string of characters from the arrays */
-  for (i = 0; i < passwordLength - 2; i++) {
+  for (i = 0; i < passwordLength - 5; i++) {
    password.push(getRandom(allchars));
   }
-  
   return password.join("")
-}
+  }
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
